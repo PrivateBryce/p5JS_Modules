f3db4ffb3213a5400983d20a0cfc3b9846a159fe
SpooksHD <ChewbaccaUniverse@gmail.com>
2017-11-29 18:37:32 -0800

    Camera Improvements
    Simply improved camera funtionality.

--- 3DEnvironment/camera.js
+++ 3DEnvironment/camera.js
@@ -1,1 +1,34 @@
+/*
+    Author: Brycen. H. Tate
+    Created: 11/29/17
+    Primary Purpose: Camera Class for Environmental Purposes
+    Dependancies: Vector3 Class, Main.JS
+*/
 
+//Initialize Class
+var camera = function(fov,aspectRatio,nearPlane,farPlane) {
+    //Implement Properties
+    if (arguments.length === 0) {
+        this.fov = 70;
+        this.aspectRatio = width/height;
+        this.nearPlane = 0.1;
+        this.farPlane = 100;
+    }else {
+        this.fov = fov || 70;
+        this.aspectRatio = aspectRatio || width/height;
+        this.nearPlane = nearPlane || 0.1;
+        this.farPlane = farPlane || 100;
+    }
+    this.position = new Vector3();
+    this.rotation = new Vector3();
+
+    //RenderPass Cycle
+    this.renderPass.push(function() {
+        perspective(this.fov, this.aspectRatio, this.nearPlane, this.farPlane);
+        /*
+            TODO:
+                Add rotation to camera by rotating all objects around cameras position by cameras rotation.
+                Add movement of objects relative to the cameras position.
+        */
+    });
+}
