a0e3e39322559b1bfeeae4a7e5b7b67ea0c5f777
Bryce_Private <34108633+PrivateBryce@users.noreply.github.com>
2017-12-01 09:06:35 -0800

    Fixed camera issues.

--- 3DEnvironment/camera.js
+++ 3DEnvironment/camera.js
@@ -6,25 +6,26 @@
 */
 
 //Initialize Class
-var camera = function(fov,aspectRatio,nearPlane,farPlane) {
+var cameraObject = function(fov,aspectRatio,nearPlane,farPlane) {
     //Implement Properties
     if (arguments.length === 0) {
-        this.fov = 70;
+        this.fov = 120;
         this.aspectRatio = width/height;
         this.nearPlane = 0.1;
-        this.farPlane = 100;
+        this.farPlane = 1000;
     }else {
-        this.fov = fov || 70;
+        this.fov = fov || 120;
         this.aspectRatio = aspectRatio || width/height;
         this.nearPlane = nearPlane || 0.1;
-        this.farPlane = farPlane || 100;
+        this.farPlane = farPlane || 1000;
     }
     this.position = new Vector3();
     this.rotation = new Vector3();
 
     //RenderPass Cycle
+	var that = this;
     renderPass.push(function() {
-        perspective(this.fov, this.aspectRatio, this.nearPlane, this.farPlane);
+				perspective(Math.rad(that.fov), that.aspectRatio, that.nearPlane, that.farPlane);
         /*
             TODO:
                 Add rotation to camera by rotating all objects around cameras position by cameras rotation.
