587813479b50fbb4316eb54d438368d481e7a830
Bryce_Private <34108633+PrivateBryce@users.noreply.github.com>
2017-12-05 14:09:05 -0800

    Added colors to objects

--- 3DEnvironment/object.js
+++ 3DEnvironment/object.js
@@ -7,11 +7,12 @@
 */
 
 //Initialize Class
-var object = function(type,size,position,rotation) {
+var object = function(type,size,position,rotation,rgb) {
 	this.size = size || new Vector3(25,25,25);
 	this.position = position || new Vector3();
 	this.rotation = rotation || new Vector3();
 	this.type = type || "cube";
+	this.color = rgb || new rgba(48,48,48);
 
 	var that = this;
 	renderPass.push(function() {
@@ -19,7 +20,7 @@ var object = function(type,size,position,rotation) {
 		rotateX(that.rotation.x);
 		rotateY(that.rotation.y);
 		rotateZ(that.rotation.z);
-		//specularMaterial(125,45,122);
+		specularMaterial(this.color.r,this.color.g,this.color.b,255*(1-this.color.a));
 		if(that,type==="cube") {
 			box(that.size.x,that.size.y,that.size.z);
 		}else if(that.type==="sphere") {
